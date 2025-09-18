6650ad98b221880d14cf929cf33b827d90294d40
Vladas <46981484+littletrickster@users.noreply.github.com>
2025-09-18 09:02:07 +0300

    version 2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.littletrickster.usersbooks"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunner = "com.littletrickster.usersbooks.CustomTestRunner"
